95196340fb7843dbd6ac9b91b83a595a8b4869bd
ChrisOrdi <chris.vandalen@ordina.nl>
2024-01-31 10:01:08 +0100

    add product func

--- src/main/java/com/techretail/ecommerce/ecommercebackend/controller/ProductController.java
+++ src/main/java/com/techretail/ecommerce/ecommercebackend/controller/ProductController.java
@@ -1,4 +1,25 @@
 package com.techretail.ecommerce.ecommercebackend.controller;
 
+
+import com.techretail.ecommerce.ecommercebackend.model.Product;
+import com.techretail.ecommerce.ecommercebackend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    // get all products
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
 }
--- src/main/java/com/techretail/ecommerce/ecommercebackend/repository/ProductRepository.java
+++ src/main/java/com/techretail/ecommerce/ecommercebackend/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package com.techretail.ecommerce.ecommercebackend.repository;
 
-public class ProductRepository {
+import com.techretail.ecommerce.ecommercebackend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
 }
